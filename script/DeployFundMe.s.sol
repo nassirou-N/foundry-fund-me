1e5d0bba9f0d4df4805d6f95babe1438a389bef5
nassirou <nassirou9993@gmail.com>
2024-11-14 17:09:33 +0100

    the format correct

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,8 +6,7 @@ import {Script, console} from "forge-std/Script.sol";
 import "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-
-    function run() external returns(address){
+    function run() external returns (address) {
         vm.startBroadcast();
         FundME fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.26;
+
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    
     //address internal constant addresData = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
-   /// AggregatorV3Interface internal dataFeed = AggregatorV3Interface(addresData);
+    /// AggregatorV3Interface internal dataFeed = AggregatorV3Interface(addresData);
 
-  //  constructor() {
-   //  dataFeed = 
-  //  }
-   function getPrice(AggregatorV3Interface dataFeed) internal view returns (uint256) {
+    //  constructor() {
+    //  dataFeed =
+    //  }
+    function getPrice(AggregatorV3Interface dataFeed) internal view returns (uint256) {
         (, int256 price,,,) = dataFeed.latestRoundData();
         return uint256(price * 1e10);
     }
 
-    function getConversionRate(uint256 etherAmount, AggregatorV3Interface addresData ) internal view returns (uint256) {
+    function getConversionRate(uint256 etherAmount, AggregatorV3Interface addresData) internal view returns (uint256) {
         uint256 etherPrice = getPrice(addresData);
 
-        return(etherPrice * etherAmount) / 1e18;
+        return (etherPrice * etherAmount) / 1e18;
     }
-  
 }
